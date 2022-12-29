d705c6dd6787b5c5679351b599ff093b84d3fa5d
Slivak Evgen <eas1804@gmail.com>
2022-12-29 11:02:12 +0200

    File Mount - add change local folder

--- sh/net_drive/1-Mount_drive.sh
+++ sh/net_drive/1-Mount_drive.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Что монтироват
+# Что монтировать - данные с сетевого диска
 IP=192.168.70.30
 FOLDER_NET=Soft
-# Куда монтировать
-FOLDER_LOCAL=//mnt/netdrive
+
+# Куда монтировать. Данные с локальлного диска
+FOLDER_LOCAL=/mnt/netdrive/soft
+
+########################     CONSTANTA     ################################
+
+# Проверить, существует ли каталог, куда монтировать. Если его нет, то он будет создан
+ls  $FOLDER_LOCAL || mkdir -p $FOLDER_LOCAL
+#Монтирование
 mount -t cifs //$IP/$FOLDER_NET $FOLDER_LOCAL -o  credentials=/etc/credential  -o  vers=3.0
+
 # Показать результат
 echo ----REZULTAT--------
 df -h | grep /mnt
+
 exit 0
